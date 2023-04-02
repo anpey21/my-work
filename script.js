77cfb38f09d635d008ec83741004c07d7d20c9ff
Anna Peysikova <anpey21@gmail.com>
2023-04-02 19:02:34 +0700

    sunday last commit

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const menu = document.getElementsByClassName('menu')[0]
-
-// Code to begin with the menu hidden and open it when the button is clicked
-
-
+const toggleButton = document.querySelector('.toggle-button');
+const menu = document.querySelector('.menu');
 
 toggleButton.addEventListener('click', () => {
-  menu.classList.toggle('active')
-})
-
-// How to close the menu when a link is clicked
-
-const menuLinks = document.querySelectorAll('.menu a')
-menuLinks.forEach(
-  function(menuLink) {
-    menuLink.addEventListener('click', () => {
-      menu.classList.remove('active')
-    })
-  }
-)
-
+  let menuLinks = document.querySelectorAll('.menu a');
+  menuLinks.forEach((link) => {
+  link.classList.toggle('active');
+  });
+});
