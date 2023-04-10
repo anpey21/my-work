c20def100043698557077c888d77255d231369a9
Anna Peysikova <anpey21@gmail.com>
2023-04-10 16:45:14 +0700

    toggle kind of works

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const toggleButton = document.querySelector('.toggle-button');
-const menu = document.querySelector('.menu');
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const sidebar = document.getElementsByClassName('sidebar')[0]
+
+// When you click the toggleButton, the sidebar will appear. When clicked again, the sidebar will disappear.
+
+//add active, then remove it when clicked again
 
 toggleButton.addEventListener('click', () => {
-  let menuLinks = document.querySelectorAll('.menu a');
-  menuLinks.forEach((link) => {
-  link.classList.toggle('active');
-  });
-});
+  sidebar.classList.toggle('active')
+})
+
+
+// toggleButton.addEventListener('click', () => {
+//   const isActive = sidebar.classList.contains('active')
+//   if (isActive) {
+//     sidebar.classList.remove('active')
+//   }
+//   sidebar.classList.toggle('active')
+// }
+// )
