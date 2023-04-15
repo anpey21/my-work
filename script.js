1ca4d775a75d189e75f2d4958458507e600061cd
Anna Peysikova <anpey21@gmail.com>
2023-04-15 21:54:26 +0700

    s

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
+// Make the toggle button display the sidebar
+// and hide the sidebar when the close button is clicked
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const sidebar = document.getElementsByClassName('sidebar')[0]
 
-// When you click the toggleButton, the sidebar will appear. When clicked again, the sidebar will disappear.
-
-//add active, then remove it when clicked again
-
-toggleButton.addEventListener('click', () => {
-  sidebar.classList.toggle('active')
-})
-
-
-// toggleButton.addEventListener('click', () => {
-//   const isActive = sidebar.classList.contains('active')
-//   if (isActive) {
-//     sidebar.classList.remove('active')
-//   }
-//   sidebar.classList.toggle('active')
-// }
-// )
+toggleButton.addEventListener('change', () => {
+  if (toggleButton.checked) {
+    sidebar.classList.add('active');
+  } else {
+    sidebar.classList.remove('active');
+  }
+});
